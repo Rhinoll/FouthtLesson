0a377ad15b94707c86fe1304bb3cd870313f7697
Andrew <zenit.yfon@gmail.com>
2021-10-22 14:54:21 +0300

    First

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        String s = "545";
+        int [] i = new int[56];
+        Person e = new Person();
+        System.out.println(e.some);
+        e.some += 78;
+        System.out.println(e.some);
+        Person e2 = e;
+        System.out.println(e2.some);
+        change(e2);
+        System.out.println(e2.some);
+        System.out.println(new Person().some);
+    }
+    private static void change(Person ex){
+        ex.some /= 2;
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,4 @@
+package com.company;
+public class Person {
+    int some = 87;
+}
